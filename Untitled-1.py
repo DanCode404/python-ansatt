de6a5ab3760e32f2eb05adf359ac067138878767
DanCode404 <remixer.za@gmail.com>
2022-11-02 09:05:34 +0100

    lol

---
+++ Untitled-1.py
@@ -0,0 +1,67 @@
+import json
+
+ansatte = '''
+{
+    "navn": "Daniel",
+    "etternavn": "Myrvoll",
+    "stilling": "vasker"
+}
+'''
+
+data = json.loads(ansatte)
+
+new_string = json.dumps(data, indent=2)
+
+def add():
+    print("Legg inn ny ansatt")
+    print("")
+    lol = input("Ansatt navn: ")
+
+def edit():
+    print("Rediger ansatt")
+    print("")
+    lol = input("Hvem vil du redigere: ")
+
+def remove():
+    print("Fjern ansatt")
+    print("")
+    lol = input("Hvem vil du fjerne: ")
+
+def close():
+    exit
+
+def lister():
+    print(data["name"], data["age"])
+
+def main():
+    print("BlackTure ansatt liste")
+    print("")
+    print("1. Legg inn ny ansatt")
+    print("2. Rediger ansatt")
+    print("3. Slett ansatt")
+    print("4. List ut alle ansatte")
+    print("5. Avslutt")
+    print("")
+    
+    lol = input("Hva vil du gjøre?:")
+    
+    if lol == "1":
+        add()
+
+    if lol == "2":
+        edit()
+
+    if lol == "3":
+        remove()
+
+    if lol == "4":
+        lister()
+
+    if lol == "5":
+        close()
+
+
+main()
+
+
+
