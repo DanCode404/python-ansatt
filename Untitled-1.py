43a46ccb51d76873d3198e651686e3405f293449
DanCode404 <remixer.za@gmail.com>
2022-11-09 23:55:48 +0100

    Får ikke til med json :(
    oppdateringen

--- Untitled-1.py
+++ Untitled-1.py
@@ -9,60 +9,80 @@ ansatte = '''
 '''
 
 
+def leggtil():
+    print("Skriv inn navnet")
+    kontroll_n = input("> ")
+    print("Skriv inn etternavn")
+    kontroll_en = input("> ")
+    print("Skriv inn stilling")
+    kontroll_s = input("> ")
+    print("Skal", kontroll_s, kontroll_n, kontroll_en, "legges til? -  j/n" )
+    kontroll_legger = input("> ")
+    nyansatt = {0: {"fornavn": kontroll_n, "etternavn": kontroll_en, "stilling": kontroll_s}}
 
-data = json.loads(ansatte)
-
-
-def add():
-    print("Legg inn ny ansatt")
-    print("")
-    lol = input("Ansatt navn: ")
+    if kontroll_legger == "n":
+        print("Sender deg tilbake til meny")
+        time.sleep(0.3)
+        print(".")
+        time.sleep(0.3)
+        print("..")
+        time.sleep(0.3)
+        print("...")
+        time.sleep(0.3)
+        print("....")
+        main()
+    if kontroll_legger == "j":
+        with open("save.json", "w") as f:
+            json.dump(nyansatt, f)
+    main()
 
 def edit():
     print("Rediger ansatt")
     print("")
     lol = input("Hvem vil du redigere: ")
+    main()
 
 def remove():
     print("Fjern ansatt")
     print("")
     lol = input("Hvem vil du fjerne: ")
+    main()
 
 def close():
     exit
 
 def lister():
     print(data["name"], data["age"])
-
-def main():
-    print("BlackTure ansatt liste")
-    print("")
-    print("1. Legg inn ny ansatt")
-    print("2. Rediger ansatt")
-    print("3. Slett ansatt")
-    print("4. List ut alle ansatte")
-    print("5. Avslutt")
-    print("")
-    
-    lol = input("Hva vil du gjøre?:")
-    
-    if lol == "1":
-        add()
-
-    if lol == "2":
-        edit()
-
-    if lol == "3":
-        remove()
-
-    if lol == "4":
-        lister()
-
-    if lol == "5":
-        close()
-
-
     main()
 
+def main():
+    print("Ansatt Register")
+    print("")
+    print("1. Legg inn ansatt")
+    print("2. Fjern ansatt")
+    print("3. Rediger ansatte")
+    print("4. List ansatte")
+    print("5. Close")
+
+    kontroll = input("> ")
+
+    if kontroll == "1":
+        leggtil()
+    
+    if kontroll == "2":
+        fjerner()
+
+    if kontroll == "3":
+        edit()
+    
+    if kontroll == "4":
+        listut()
 
 
+main()
+
+
+import json
+import time
+
+ansatt = {"stillingsnummer": 0, 0: {"fornavn": "Daniel", "etternavn": "myrvoll", "jobb": "vasker"}}
